f5d57062657a61fc2886c5b3ce84d2523640d6be
clifford-peeples <33702784+clifford-peeples@users.noreply.github.com>
2017-11-15 20:19:05 -0500

    Updated method, too large of a margin for error in last iteration

--- mips1.asm
+++ mips1.asm
@@ -1,16 +1,17 @@
 .data
     message: .asciiz "Enter a string. "
-    userInput: .space 8
+    userInput: .space 9
 .text
     main:
-        #Getting using input as text
-        li $v0, 8
-        la $a0, userInput
-        li $a1, 9
-        syscall
+      j readChar
+        gets:           #read multiple chars from keyboard buffer until ENTER key, add NULL char and store into buffer pointed to by *array passed to the subroutine
+            la $s1, array       #set base address of array to s1
         
-        #
+    readInt:        #read string of ascii digits, store into a local variable, convert into integer, return that int
+        j gets          #let s1 be top address of array, let s0 be the digitcounter
         
+    gets:           #read multiple chars from keyboard buffer until ENTER key
+        la $s1, array       #set base address of array to s1
         
         
         
