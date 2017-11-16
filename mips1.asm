48953d050c0805dd98be6a610fe2de3920f7ed59
clifford-peeples <33702784+clifford-peeples@users.noreply.github.com>
2017-11-15 23:43:52 -0500

    Only prints 0

--- mips1.asm
+++ mips1.asm
@@ -1,16 +1,21 @@
 .data
-    message: .asciiz "Enter a string. "
+    message: .asciiz "Invalid hexadecimal number."
     userInput: .space 9
+    char: .space 2
     newLine: .asciiz "\n"
+    null: .asciiz ""
+    space: .ascii " "
 .text
     main:
-      j readChar
-      
-    readChar:        #read string of ascii digits, store into a local variable, convert into integer, return that int
-        j gets          #let s1 be top address of array, let s0 be the digitcounter
+        la $a0, userInput
+        li $v0, 8
+        li $a1, 9
+        syscall
+        j gets
         
     gets:           #read multiple chars from keyboard buffer until ENTER key
-        la $s1, array       #set base address of array to s1
+        la $s1, userInput       #set base address of array to s1
+        
     loopStart:           #start of read loop
         jal getChar        #jump to getc subroutine
         lb $t0, char        #load the char from char buffer into t0, stripping null
@@ -29,44 +34,62 @@
         
     done:           #let s2 be the sum total
         addi $s1, $s1, -1   #reposition array pointer to last char before newline char
-        la $s0, array       #set base address of array to s0 for use as counter
+        la $s0, userInput       #set base address of array to s0 for use as counter
         addi $s0, $s0, -1   #reposition base array to read leftmost char in string
         add $s2, $zero, $zero   #initialize sum to 0
         li $t0, 10      #set t0 to be 10, used for decimal conversion
         li $t3, 1
         lb $t1, 0($s1)      #load char from array into t1
-        blt $t1, 48, error  #check if char is not a digit (ascii<'0')
-        bgt $t1, 57, error  #check if char is not a digit (ascii>'9')
-        addi $t1, $t1, -48  #converts t1's ascii value to dec value
-        add $s2, $s2, $t1   #add dec value of t1 to sumtotal
-        addi $s1, $s1, -1   #decrement array address
+        blt $t1, 48, invalid  #if less than 48 invalid
+        blt $t1, 58, validNum  #if less than 58 valid
+        blt $t1, 65, invalid #if less than 65 invalid
+        blt $t1, 71, validUpper #if less than 71 valid
+        blt $t1, 97, invalid #if less than 97 invalid
+        blt $t1, 103, validLow #if less than 103 valid
+
         
     loopSec:         #loop for all digits preceeding the LSB
         mul $t3, $t3, $t0   #multiply power by 10
         beq $s1, $s0, END   #exit if beginning of string is reached
         lb $t1, ($s1)       #load char from array into t1
-        blt $t1, 48, error  #check if char is not a digit (ascii<'0')
-        bgt $t1, 57, error  #check if char is not a digit (ascii>'9')
+        blt $t1, 48, invalid  #if less than 48 invalid
+        blt $t1, 58, validNum  #if less than 58 valid
+        blt $t1, 65, invalid #if less than 65 invalid
+        blt $t1, 71, validUpper #if less than 71 valid
+        blt $t1, 97, invalid #if less than 97 invalid
+        blt $t1, 103, validLow #if less than 103 valid
+        j loopSec            #jump to start of loop
+
+    invalid:          #if non digit chars are entered, readInt returns 0
+        add $s2, $zero, $zero
+        li $v0, 4
+        la $a0, message
+        j END
+        
+    validNum:
         addi $t1, $t1, -48  #converts t1's ascii value to dec value
         mul $t1, $t1, $t3   #t1*10^(counter)
         add $s2, $s2, $t1   #sumtotal=sumtotal+t1
         addi $s1, $s1, -1   #decrement array address
         j loopSec            #jump to start of loop
-
-    error:          #if non digit chars are entered, readInt returns 0
-        add $s2, $zero, $zero
-        j END
+        
+    validUpper:
+        addi $t1, $t1, -55  #converts t1's ascii value to dec value
+        mul $t1, $t1, $t3   #t1*10^(counter)
+        add $s2, $s2, $t1   #sumtotal=sumtotal+t1
+        addi $s1, $s1, -1   #decrement array address
+        j loopSec            #jump to start of loop
+        
+    validLow:
+        addi $t1, $t1, -93  #converts t1's ascii value to dec value
+        mul $t1, $t1, $t3   #t1*10^(counter)
+        add $s2, $s2, $t1   #sumtotal=sumtotal+t1
+        addi $s1, $s1, -1   #decrement array address
+        j loopSec            #jump to start of loop
 
     END:
         li $v0, 1
         add $a0, $s2, $zero
         syscall
-        
-        
-        
-        
-        
-        
-    #end of main
-    li $v0, 10
-    syscall
+        li $v0, 10
+        syscall
